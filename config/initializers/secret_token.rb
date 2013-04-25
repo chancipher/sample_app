9dba319ca45a7626e70642ebd3862171ba0e7771
chancipher <chan.cipher@gmail.com>
2013-04-25 17:40:22 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5f45d0445e45d552e12495f0ed568c10af94f16db8dfd692d81b988c76fa7ee083b28ae5955f95c5940942770c1a8048beeee6b89b16b2142004e43dd35f2333'
